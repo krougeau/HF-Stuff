34a15aa09dab370844d410f44cf271fb2e405a86
krougeau <krougeau@gmail.com>
2018-11-18 06:25:48 -0800

    Update audio-test.js

--- code/audio-test.js
+++ code/audio-test.js
@@ -7,7 +7,7 @@ var sound = SoundCache.getSound(Script.resourcesPath() + "sounds/test.mp3");
 var injector;
 
 Script.setTimeout(function () { // Give the sound time to load.
-  injector = Audio.playSound(sound, injectorOptions);
+  injector = Audio.playSound(sound);
 }, 1000);
 
 var entityID = Entities.addEntity({
