57c45925af7759032c97fb5fdb6119410abc97df
krougeau <krougeau@gmail.com>
2018-12-24 15:40:53 -0800

    Delete audio-test-2.js

--- code/audio-test-2.js
+++
@@ -1,6 +0,0 @@
-var sound = SoundCache.getSound(Script.resourcesPath() + "sounds/test.mp3");
-var injector;
-
-Script.setTimeout(function () { // Give the sound time to load.
-  injector = Audio.playSound(sound, injectorOptions);
-}, 1000);
