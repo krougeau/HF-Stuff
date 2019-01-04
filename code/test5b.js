129a66a73c27f9730b384314012941ded393fee2
krougeau <krougeau@gmail.com>
2019-01-04 05:21:59 -0800

    Add files via upload

---
+++ code/test5b.js
@@ -0,0 +1,22 @@
+(function(){ 
+    var childObject;
+    this.preload = function(entityID) {
+        print("(ID: " + entityID + "): Initialized. Listening for user entries.");
+        childObject = Entities.getChildrenIDs(entityID);
+        print("Children: " + JSON.stringify(childObject));  // Only the child entity.
+    }
+
+    this.enterEntity = function(entityID) {        
+	print("Entered");
+	print("ParentID = " + MyAvatar.getParentID());
+        if(MyAvatar.getParentID() != childObject){
+              MyAvatar.setParentID(childObject);
+          }
+	}; 
+    
+    this.leaveEntity = function(entityID) {    
+	print("Exited");
+	print("ParentID = " + MyAvatar.getParentID());
+        MyAvatar.setParentID(0);
+    };    
+})
