2ea16c8be3c9c292ad8b3733e2d668262fa6cae2
krougeau <krougeau@gmail.com>
2019-01-03 23:09:35 -0800

    Add files via upload

---
+++ code/parent-test.js
@@ -0,0 +1,13 @@
+
+    this.enterEntity = function(entityID) {
+	if (MyAvatar.getParentID() != this) {
+            MyAvatar.position = { x: 0, y: 3, z: 0 };
+	    MyAvatar.setParentID(this);
+            print("enterEntity() .... Entrances: " + ents);
+		}
+	}; 
+    
+    this.leaveEntity = function(entityID) {
+      	MyAvatar.setParentID(0);
+	print("enterEntity() .... Exits: " + exts);
+    };    
