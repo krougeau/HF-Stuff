56fe88cc484ca0a423a99892202d66aa5b8d0a06
krougeau <krougeau@gmail.com>
2019-07-12 20:20:24 -0700

    Update reset_test_01.js

--- code/reset_test_01.js
+++ code/reset_test_01.js
@@ -1,8 +1,9 @@
 (function () {
 
-var scanPosition = {x: 0, y: 0 , z:0};
+ar scanPosition = {x: 0, y: 0 , z:0};
 var npcs = Entities.findEntitiesByName("NPC", scanPosition, 10000, false);
 var startPos = new Array();
+var controller = Entities.findEntitiesByName("StartStop Button", scanPosition, 10000, false);
 
 print("(RESET) Number of NPCs: " + npcs.length);
 
@@ -11,5 +12,9 @@ for (i = 0; i < npcs.length; i++)
 	  startPos.push(Entities.getEntityProperties(npcs[i]).position);
 	  print("(RESET) NPC #" + i + " started at " + JSON.stringify(startPos[i]));
 	};
+
+this.clickDownOnEntity = function (entityID, mouseEvent) {
+        Entities.callEntityMethod(controller, "externalCall");       
+    };
 
 });
