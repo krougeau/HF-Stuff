a6d1db1c052d64b88ece0ae45d40e3083f4dc5f9
krougeau <krougeau@gmail.com>
2019-07-12 20:26:22 -0700

    Update npc-test_05.js

--- code/npc-test_05.js
+++ code/npc-test_05.js
@@ -3,6 +3,7 @@
 var started = false;
 var scanPosition = {x: 0, y: 0 , z:0};
 var npcs = Entities.findEntitiesByName("NPC", scanPosition, 10000, false);
+public var externalCall = externalFunction();
 
 print("Number of NPCs: " + npcs.length);
 
@@ -51,7 +52,7 @@ function fixColor()
 	  Entities.editEntity(this, { color: { red: 0, green: 0, blue: 255} });
 	  print("Fixing color.");
 	}
-function externalCall()
+function externalFunction()
 	{
 	  print("IT WORKED!!!!");
 	}
