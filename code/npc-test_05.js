9be70f87ddc73d9e9faf928a2722d7b282f220d5
krougeau <krougeau@gmail.com>
2019-07-12 19:26:52 -0700

    Update npc-test_05.js

--- code/npc-test_05.js
+++ code/npc-test_05.js
@@ -14,10 +14,22 @@ for (i = 0; i < npcs.length; i++)
 
 this.clickDownOnEntity = function (entityID, mouseEvent) {
         if (started){
+	  for (i = 0; i < npcs.length; i++)
+	  {
+	    Entities.editEntity(npcs[i], { linearVelocity: { x: 0.5, y: 0, z: 0} });
+	    var vel = Entities.getEntityProperties(npcs[i]).linearVelocity;
+	    print("NPC #" + i + " is moving at " + JSON.stringify(vel));
+	  };
             Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 0} });
             started = false;
             print ("Stopping.");
         } else {
+	  for (i = 0; i < npcs.length; i++)
+	  {
+	    Entities.editEntity(npcs[i], { linearVelocity: { x: 0, y: 0, z: 0} });
+	    var vel = Entities.getEntityProperties(npcs[i]).linearVelocity;
+	    print("NPC #" + i + " is moving at " + JSON.stringify(vel));
+	  };
             Entities.editEntity(entityID, { color: { red: 255, green: 0, blue: 0} });
             started = true;
             print ("Starting.");
