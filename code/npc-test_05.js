9f7f2da936edd6d9e7683d21d8a15bab2865e27e
krougeau <krougeau@gmail.com>
2019-07-12 20:57:54 -0700

    Update npc-test_05.js

--- code/npc-test_05.js
+++ code/npc-test_05.js
@@ -2,7 +2,14 @@
 
 var started = false;
 var scanPosition = {x: 0, y: 0 , z:0};
+
 var npcs = Entities.findEntitiesByName("NPC", scanPosition, 10000, false);
+var startPos = new Array();
+
+var resetButtons = Entities.findEntitiesByName("Reset Button", scanPosition, 10000, false);
+var resetButton = resetButtons[0];
+
+var _this = this;
 
 print("Number of NPCs: " + npcs.length);
 
@@ -22,7 +29,7 @@ this.clickDownOnEntity = function (entityID, mouseEvent) {
 	  };
             Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 0} });
             started = false;
-            print ("Stopping.");
+            print ("Starting.");
         } else {
 	  for (i = 0; i < npcs.length; i++)
 	  {
@@ -32,28 +39,28 @@ this.clickDownOnEntity = function (entityID, mouseEvent) {
 	  };
             Entities.editEntity(entityID, { color: { red: 255, green: 0, blue: 0} });
             started = true;
-            print ("Starting.");
+            print ("Stopping.");
         }
     };
 	
 this.preload = function () {
         print("Preloading Start/Stop Button");
-	fixColor();
+	Entities.editEntity(_this, { color: { red: 0, green: 0, blue: 255} });
     }
 
 this.unload = function () {
 	print("Unloading Start/Stop Button");
-	fixColor();
+	Entities.editEntity(_this, { color: { red: 0, green: 0, blue: 255} });
     }
-	
-function fixColor()
-	{
-	  Entities.editEntity(this, { color: { red: 0, green: 0, blue: 255} });
-	  print("Fixing color.");
-	}
 
-var externalCall = (function () {
-	print("IT WORKED!!!!");
-  });
+resetButton.clickDownOnEntity = function (entityID, mouseEvent) {
+	  for (i = 0; i < npcs.length; i++)
+	  {
+	    Entities.editEntity(npcs[i], { velocity: { x: 0.0, y: 0, z: 0} });
+	    Entities.editEntity(npcs[i], { position: {x: startPos[i].x, y: startPos[i].y, z: startPos[i].z} });
+	  };
+	Entities.editEntity(_this, { color: { red: 255, green: 0, blue: 0} });
+	started = false;	
+  };
 	
 });
