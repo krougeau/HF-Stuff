9d40c110c55f2f31e530919dcd31878a5bc3b912
krougeau <krougeau@gmail.com>
2019-07-12 19:11:54 -0700

    Add files via upload

---
+++ code/npc-test_05.js
@@ -0,0 +1,27 @@
+(function () {
+
+var started = false;
+var scanPosition = {x: 0, y: 0 , z:0};
+var npcs = Entities.findEntitiesByName("NPC", scanPosition, 10000, false);
+
+print("Number of NPCs: " + npcs.length);
+
+for (i = 0; i < npcs.length; i++)
+	{
+	  var startPos = Entities.getEntityProperties(npcs[i]).position;
+	  print("NPC #" + i + " is located at " + JSON.stringify(startPos));
+	};
+
+this.clickDownOnEntity = function (entityID, mouseEvent) {
+        if (started){
+            Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 0} });
+            started = false;
+            print ("Stopping.");
+        } else {
+            // Entities.editEntity(entityID, { color: { red: 255, green: 0, blue: 0} });
+            started = true;
+            print ("Starting.");
+        }
+    };
+
+});
