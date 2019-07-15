684cb355513dd48fb0c5438d6cd8406bfe108726
krougeau <krougeau@gmail.com>
2019-07-15 04:21:39 -0700

    Add files via upload

---
+++ code/reset_test_03.js
@@ -0,0 +1,11 @@
+(function () {
+
+var scanPosition = {x: 0, y: 0 , z:0};
+var controllerIDs = Entities.findEntitiesByName("StartStop Controller", scanPosition, 10000, false);
+
+this.clickDownOnEntity = function (entityID, mouseEvent) {
+        // print("Clickity click!");
+	Entities.callEntityServerMethod(controllerIDs[0], "resetNPCpositions");       
+    };
+
+});
