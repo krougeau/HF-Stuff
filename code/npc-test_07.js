d7c369cb5afe2a999f8c2f53d47e7837ee2a0057
krougeau <krougeau@gmail.com>
2019-07-15 05:59:29 -0700

    Update npc-test_07.js

--- code/npc-test_07.js
+++ code/npc-test_07.js
@@ -1,4 +1,4 @@
-(function () {    
+(function () {
     var DEBUGGING = true;
     var _this = this;
     var SEARCH_RADIUS = 10000.0;
@@ -36,47 +36,61 @@
         y: 0.0,
         z: -0.5
     };
-
     var NO_VELOCITY = {
         x: 0.0,
         y: 0.0,
         z: 0.0
     };
+    var WALKING_ANIMATION = {
+        "animation": {
+            "url": "https://github.com/krougeau/HF-Stuff/raw/master/nny/Johnny_Walking.fbx",
+            "fps": 45,
+            "running": true
+        }
+    };
+
+    var IDLE_ANIMATION = {
+        "animation": {
+            "url": "https://github.com/krougeau/HF-Stuff/raw/master/h22/hitc-22_breathing-idle.fbx",
+            "fps": 45,
+            "running": true
+        }
+    };
 
     var started = false;
     var SEARCH_CENTER = Vec3.ZERO;
-    
+
     var npc_east_IDs = Entities.findEntitiesByName("NPC_East", SEARCH_CENTER, SEARCH_RADIUS, !CASE_SENSITIVE);
     var npc_west_IDs = Entities.findEntitiesByName("NPC_West", SEARCH_CENTER, SEARCH_RADIUS, !CASE_SENSITIVE);
     var npc_north_IDs = Entities.findEntitiesByName("NPC_North", SEARCH_CENTER, SEARCH_RADIUS, !CASE_SENSITIVE);
     var npc_south_IDs = Entities.findEntitiesByName("NPC_South", SEARCH_CENTER, SEARCH_RADIUS, !CASE_SENSITIVE);
-    
+
     var NPCstartPositions_East = [];
     var NPCstartPositions_West = [];
     var NPCstartPositions_North = [];
     var NPCstartPositions_South = [];
-    
+
     var NPCendPositions_East = [];
     var NPCendPositions_West = [];
     var NPCendPositions_North = [];
     var NPCendPositions_South = [];
 
-    var NPC_Controller = function(){        
+    var NPC_Controller = function () {
     };
 
     NPC_Controller.prototype = {
         remotelyCallable: ["checkNPCpositions", "resetNPCpositions", "startStopNPCs"],
-        startStopNPCs: function(id) {
+        startStopNPCs: function (id) {
             startStop();
         },
-        resetNPCpositions: function(id) {
+        resetNPCpositions: function (id) {
             resetPositions();
         },
-        checkNPCpositions: function(id){
+        checkNPCpositions: function (id) {
             checkPositions();
         }
     }
-    
+
     debugPrint("Number of Eastbound NPCs: " + npc_east_IDs.length);
     debugPrint("Number of Westbound NPCs: " + npc_west_IDs.length);
     debugPrint("Number of Northbound NPCs: " + npc_north_IDs.length);
@@ -140,7 +154,7 @@
         NPCendPositions_South.push(npcPosition);
     });
 
-    function startStop (id) {
+    function startStop(id) {
         debugPrint("START STOP WAS CALLED");
         var i;
         if (started) {
@@ -148,24 +162,28 @@
                 var currentNPC = npc_east_IDs[i];
                 setNPCvelocity(currentNPC, STATIONARY);
                 NPCendPositions_East[i] = getNPCposition(currentNPC);
+                setAnimation(currentNPC, IDLE_ANIMATION);
                 debugPrint("NPC #" + i + " stopped at " + JSON.stringify(NPCendPositions_East[i]));
             }
             for (i = 0; i < npc_west_IDs.length; i++) {
                 var currentNPC = npc_west_IDs[i];
                 setNPCvelocity(currentNPC, STATIONARY);
                 NPCendPositions_West[i] = getNPCposition(currentNPC);
+                setAnimation(currentNPC, IDLE_ANIMATION);
                 debugPrint("NPC #" + i + " stopped at " + JSON.stringify(NPCendPositions_West[i]));
             }
             for (i = 0; i < npc_north_IDs.length; i++) {
                 var currentNPC = npc_north_IDs[i];
                 setNPCvelocity(currentNPC, STATIONARY);
                 NPCendPositions_North[i] = getNPCposition(currentNPC);
+                setAnimation(currentNPC, IDLE_ANIMATION);
                 debugPrint("NPC #" + i + " stopped at " + JSON.stringify(NPCendPositions_North[i]));
             }
             for (i = 0; i < npc_south_IDs.length; i++) {
                 var currentNPC = npc_south_IDs[i];
                 setNPCvelocity(currentNPC, STATIONARY);
                 NPCendPositions_South[i] = getNPCposition(currentNPC);
+                setAnimation(currentNPC, IDLE_ANIMATION);
                 debugPrint("NPC #" + i + " stopped at " + JSON.stringify(NPCendPositions_South[i]));
             }
             setEntityColour(_this, RED);
@@ -175,21 +193,25 @@
             for (i = 0; i < npc_east_IDs.length; i++) {
                 setNPCvelocity(npc_east_IDs[i], NPC_EAST_DEFAULT_VELOCITY);
                 var currentVelocity = JSON.stringify(getNPCvelocity(npc_east_IDs[i]));
+                setAnimation(currentNPC, WALKING_ANIMATION);
                 debugPrint("NPC #" + i + " is moving at velocity " + currentVelocity);
             }
             for (i = 0; i < npc_west_IDs.length; i++) {
                 setNPCvelocity(npc_west_IDs[i], NPC_WEST_DEFAULT_VELOCITY);
                 var currentVelocity = JSON.stringify(getNPCvelocity(npc_west_IDs[i]));
+                setAnimation(currentNPC, WALKING_ANIMATION);
                 debugPrint("NPC #" + i + " is moving at velocity " + currentVelocity);
             }
             for (i = 0; i < npc_north_IDs.length; i++) {
                 setNPCvelocity(npc_north_IDs[i], NPC_NORTH_DEFAULT_VELOCITY);
                 var currentVelocity = JSON.stringify(getNPCvelocity(npc_north_IDs[i]));
+                setAnimation(currentNPC, WALKING_ANIMATION);
                 debugPrint("NPC #" + i + " is moving at velocity " + currentVelocity);
             }
             for (i = 0; i < npc_south_IDs.length; i++) {
                 setNPCvelocity(npc_south_IDs[i], NPC_SOUTH_DEFAULT_VELOCITY);
                 var currentVelocity = JSON.stringify(getNPCvelocity(npc_south_IDs[i]));
+                setAnimation(currentNPC, WALKING_ANIMATION);
                 debugPrint("NPC #" + i + " is moving at velocity " + currentVelocity);
             }
             setEntityColour(_this, GREEN);
@@ -211,8 +233,8 @@
         started = false;
     };
 
-    function resetPositions (entityID) {
-        var i;
+    function resetPositions(entityID) {
+        var i;        
         for (i = 0; i < npc_east_IDs.length; i++) {
             Entities.editEntity(npc_east_IDs[i], {
                 velocity: Vec3.ZERO
@@ -220,6 +242,7 @@
             Entities.editEntity(npc_east_IDs[i], {
                 position: NPCstartPositions_East[i]
             });
+            setAnimation(npc_east_IDs[i], IDLE_ANIMATION);
             debugPrint("NPC #" + i + " returned to " + JSON.stringify(NPCstartPositions_East[i]));
         }
         for (i = 0; i < npc_west_IDs.length; i++) {
@@ -229,6 +252,7 @@
             Entities.editEntity(npc_west_IDs[i], {
                 position: NPCstartPositions_West[i]
             });
+            setAnimation(npc_west_IDs[i], IDLE_ANIMATION);
             debugPrint("NPC #" + i + " returned to " + JSON.stringify(NPCstartPositions_West[i]));
         }
         for (i = 0; i < npc_north_IDs.length; i++) {
@@ -238,6 +262,7 @@
             Entities.editEntity(npc_north_IDs[i], {
                 position: NPCstartPositions_North[i]
             });
+            setAnimation(npc_north_IDs[i], IDLE_ANIMATION);
             debugPrint("NPC #" + i + " returned to " + JSON.stringify(NPCstartPositions_North[i]));
         }
         for (i = 0; i < npc_south_IDs.length; i++) {
@@ -247,6 +272,7 @@
             Entities.editEntity(npc_south_IDs[i], {
                 position: NPCstartPositions_South[i]
             });
+            setAnimation(npc_south_IDs[i], IDLE_ANIMATION);
             debugPrint("NPC #" + i + " returned to " + JSON.stringify(NPCstartPositions_South[i]));
         }
         setEntityColour(entityID, RED);
@@ -254,8 +280,8 @@
         debugPrint("Reset & stopped.");
     };
 
-   _this.intervalID = Script.setInterval(function() {
-       checkPositions();
+    _this.intervalID = Script.setInterval(function () {
+        checkPositions();
     }, 500);
 
     function checkPositions() {
@@ -287,28 +313,28 @@
     }
 
     function sendNPCtoStartPosition(npcID, startPosition, direction) {
-        if(direction === "east"){
+        if (direction === "east") {
             var position = {
                 x: NPC_POSITION_MAX_X_VALUE,
                 y: startPosition.y,
                 z: startPosition.z
             };
         }
-        if(direction === "west"){
+        if (direction === "west") {
             var position = {
                 x: -NPC_POSITION_MAX_X_VALUE,
                 y: startPosition.y,
                 z: startPosition.z
             };
         }
-        if(direction === "north"){
+        if (direction === "north") {
             var position = {
                 x: startPosition.x,
                 y: startPosition.y,
                 z: -NPC_POSITION_MAX_Z_VALUE
             };
         }
-        if(direction === "south"){
+        if (direction === "south") {
             var position = {
                 x: startPosition.x,
                 y: startPosition.y,
@@ -319,25 +345,29 @@
             position: position
         });
     }
-	
+
     function setEntityColour(id, colour) {
         Entities.editEntity(id, {
             color: colour
         });
     }
-	
+
     function setNPCvelocity(NPCid, vel) {
         Entities.editEntity(NPCid, {
             velocity: vel
         });
     }
-	
+
     function getNPCvelocity(npcID) {
         return Entities.getEntityProperties(npcID, "velocity").velocity;
     }
-	
+
     function getNPCposition(npcID) {
         return Entities.getEntityProperties(npcID, "position").position;
+    }
+
+    function setAnimation(id, animation) {
+        Entities.editEntity(id, animation);
     }
 
     function debugPrint(message) {
@@ -346,6 +376,5 @@
         }
     }
 
-    // return this;
     return new NPC_Controller();
 });
