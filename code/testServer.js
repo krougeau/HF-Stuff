4a9a277a8824a5924f3ff1ff75a59ab5cf99b22a
krougeau <krougeau@gmail.com>
2019-07-15 03:31:55 -0700

    Update testServer.js

--- code/testServer.js
+++ code/testServer.js
@@ -1,4 +1,15 @@
 (function () { 
+    var toggle = false;
+    var RED = {
+        red: 255,
+        green: 0,
+        blue: 0
+    };
+    var GREEN = {
+        red: 0,
+        green: 255,
+        blue: 0
+    };
     var TestServer = function(){        
     };
 
@@ -6,7 +17,21 @@
         remotelyCallable: ["remoteCallTest"],
         remoteCallTest: function(id, params) {
             console.info("This function was called.");
+            if(toggle)                                
+            {
+                setEntityColour(id, RED);
+                toggle = !toggle;
+            } else{
+                setEntityColour(id, GREEN);
+                toggle = !toggle;
+            }
         }
+    }
+
+    function setEntityColour(id, colour) {
+        Entities.editEntity(id, {
+            color: colour
+        });
     }
 
     return new TestServer();
