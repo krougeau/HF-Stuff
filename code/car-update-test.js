31057594a90799a138799f703dc7b79fdb3cfbd1
krougeau <krougeau@gmail.com>
2019-07-20 02:26:03 -0700

    Update car-update-test.js

--- code/car-update-test.js
+++ code/car-update-test.js
@@ -1,27 +1,27 @@
 (function () {
 
     var started = false;
-    var _this = this;
+    var _id;
     print(JSON.stringify(_this));
     
     this.clickDownOnEntity = function (entityID, mouseEvent) {
+        _id = entityID;
         if(!started) {
             started = true;
             print("Started");
             print("X = " + Entities.getEntityProperties(entityID, "position").position.x);
-            print("THIS = " + JSON.stringify(_this))
         } else {
             started = false;
             print("Stopped");
             print("X = " + Entities.getEntityProperties(entityID, "position").position.x);
-            print("THIS = " + JSON.stringify(_this))
         }
     };
     
     var update = function (deltaTime) {
         if(started){
             // handle movement
-            // Entities.editEntity(_this, "position").position.x = Entities.getEntityProperties(_this, "position").position.x + (0.5 * deltaTime);
+            Entities.editEntity(_id, "position").position.x = Entities.getEntityProperties(_id, "position").position.x + (0.5 * deltaTime);
+            // print("Object should be in motion.");
         } else {
             // do nothing
         }
