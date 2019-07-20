3802b6acd4ae54727477bd9d5e5b688be7a365e8
krougeau <krougeau@gmail.com>
2019-07-20 05:32:25 -0700

    Update npc_reset_script.js

--- nny/npc_reset_script.js
+++ nny/npc_reset_script.js
@@ -2,9 +2,21 @@
 
     var scanPosition = {x: 0, y: 0 , z:0};
     var controllerIDs = Entities.findEntitiesByName("NPC Controller", scanPosition, 10000, false);
-    
+    var RED = {
+        red: 255,
+        green: 0,
+        blue: 0
+    };
+    var GREEN = {
+        red: 0,
+        green: 255,
+        blue: 0
+    };
     this.clickDownOnEntity = function (entityID, mouseEvent) {
             // print("RESETTING NPCs");
+        Entities.editEntity(id, {
+            color: RED
+        });
         Entities.callEntityServerMethod(controllerIDs[0], "resetNPCpositions");       
         };
     
